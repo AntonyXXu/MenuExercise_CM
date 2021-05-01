07745106e0bc55e364650c06cf06390ba4a73966
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-05-01 12:14:12 -0600

    improved rendering for items, added sort

--- src/index.js
+++ src/index.js
@@ -2,57 +2,66 @@ import "./styles.css";
 import menu from "./menu";
 
 const menuItems = menu.items;
-
 const types = ["starters", "pasta", "pizza"];
-
 var filteredMenu = [];
 
-const renderMenuItem = (menuItem) => {
-  const name = `<h4>${menuItem.name}</h4>`;
-  const description = `<p>${menuItem.description}</p>`;
-  const price = `<p>$${menuItem.price.toFixed(2)}</p>`;
-  const orderNumber = `<p>${menuItem.orderNumber}</p>`;
-
-  return "<p></p>" + name + description + price + orderNumber;
+const renderMenuItem = (menuItem, item) => {
+  item.innerHTML = `
+    <h4 class=${menuItem.spicy ? "spicy" : ""}>
+    ${menuItem.menuOrder} \t ${menuItem.name}
+    </h4> 
+    <p>$${menuItem.price.toFixed(2)}</p>
+    <p>${menuItem.description}</p>
+    `;
 };
 
+// Iterate through each menu item, filter by type, and sort the list.
+// Then, push the menu item to the filtered menu array.
 const sortMenu = (filteredMenu) => {
   types.forEach((type) => {
-    var menuList = menuItems.filter((menuItem) => {
-      return menuItem.type == type;
-    });
+    var menuList = menuItems
+      .filter((menuItem) => {
+        return menuItem.type == type;
+      })
+      .sort((a, b) => {
+        return a.menuOrder - b.menuOrder;
+      });
     filteredMenu.push(menuList);
   });
 };
 
+// Iterate through the filtered menu array and render its data
+// for each item. Using vanilla JS, I tried to implement something
+// similar to React, where I would render the data based on a menu item
+// 'component'.
 const renderAll = () => {
-  var i = 0;
   document.getElementById("spicyCheck").checked;
-  filteredMenu.forEach((menuType) => {
-    const listID = types[i] + "List";
+  for (var i = 0; i < filteredMenu.length; i++) {
+    const menuType = filteredMenu[i];
+    var menuSection = document.getElementById(types[i]);
     menuType.forEach((menuItem) => {
-      var item = document.createElement("li");
-      item.className = menuItem.spicy ? "spicyItem" : "";
-      console.log(menuItem.name, item.className);
-      var data = document.createTextNode(renderMenuItem(menuItem));
-      item.appendChild(data);
-      document.getElementById(listID).appendChild(item);
+      var item = document.createElement("div");
+      item.className = menuItem.spicy ? "spicyItem item" : "item";
+      renderMenuItem(menuItem, item);
+      menuSection.appendChild(item);
     });
-    i += 1;
-  });
-};
-
-const filterSpicy = () => {
-  var spicyItems = document.getElementsByClassName("spicyItem");
-  const show = document.getElementById("spicyCheck").checked;
-  console.log(show);
-  for (var i = 0; i < spicyItems.length; i++) {
-    spicyItems[i].style.display = show ? "list-item" : "none";
   }
 };
 
-document.getElementById("spicyCheck").addEventListener("click", filterSpicy);
+// Filter Spicy was created by changing the display of the spicyCheck class.
+// I had considered using another filter and return another array of all
+// menu items without the spicy property. However, this method seems less efficient
+// than just hiding the display. Not a big deal in this case with few items, but
+// could result in slower rendering with larger amounts of data.
+const filterSpicy = () => {
+  var spicyItems = document.getElementsByClassName("spicyItem");
+  const show = document.getElementById("spicyCheck").checked;
+  for (var i = 0; i < spicyItems.length; i++) {
+    spicyItems[i].style.display = show ? "block" : "none";
+  }
+};
 
+// Initialize the webpage
+document.getElementById("spicyCheck").addEventListener("click", filterSpicy);
 sortMenu(filteredMenu);
 renderAll();
-console.log(filteredMenu);
