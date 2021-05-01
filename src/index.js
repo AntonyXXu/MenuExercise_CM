57f611d41a7c284e0d3aa6f70d0fd84b28232143
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-04-30 18:58:14 -0600

    filtered loop created

--- src/index.js
+++ src/index.js
@@ -3,4 +3,21 @@ import menu from "./menu";
 
 const menuItems = menu.items;
 
-console.log(menuItems);
+const types = ["starters", "pasta", "pizza"];
+
+var filteredMenu = [];
+
+types.forEach((type) => {
+  var menuList = menuItems.filter((menuItem) => {
+    return menuItem.type == type;
+  });
+  filteredMenu.push(menuList);
+});
+
+var i = 0;
+filteredMenu.forEach((menuType) => {
+  const listID = types[i] + "List";
+  document.getElementById(listID).innerHTML = menuType;
+});
+
+console.log(filteredMenu);
