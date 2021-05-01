a4ab8ce633f038e58ab13df68a7af93d5600aa7a
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-04-30 22:18:24 -0600

    checkbox based on spicy with filtering completed

--- src/index.js
+++ src/index.js
@@ -7,17 +7,52 @@ const types = ["starters", "pasta", "pizza"];
 
 var filteredMenu = [];
 
-types.forEach((type) => {
-  var menuList = menuItems.filter((menuItem) => {
-    return menuItem.type == type;
+const renderMenuItem = (menuItem) => {
+  const name = `<h4>${menuItem.name}</h4>`;
+  const description = `<p>${menuItem.description}</p>`;
+  const price = `<p>$${menuItem.price.toFixed(2)}</p>`;
+  const orderNumber = `<p>${menuItem.orderNumber}</p>`;
+
+  return "<p></p>" + name + description + price + orderNumber;
+};
+
+const sortMenu = (filteredMenu) => {
+  types.forEach((type) => {
+    var menuList = menuItems.filter((menuItem) => {
+      return menuItem.type == type;
+    });
+    filteredMenu.push(menuList);
   });
-  filteredMenu.push(menuList);
-});
+};
 
-var i = 0;
-filteredMenu.forEach((menuType) => {
-  const listID = types[i] + "List";
-  document.getElementById(listID).innerHTML = menuType;
-});
+const renderAll = () => {
+  var i = 0;
+  document.getElementById("spicyCheck").checked;
+  filteredMenu.forEach((menuType) => {
+    const listID = types[i] + "List";
+    menuType.forEach((menuItem) => {
+      var item = document.createElement("li");
+      item.className = menuItem.spicy ? "spicyItem" : "";
+      console.log(menuItem.name, item.className);
+      var data = document.createTextNode(renderMenuItem(menuItem));
+      item.appendChild(data);
+      document.getElementById(listID).appendChild(item);
+    });
+    i += 1;
+  });
+};
 
+const filterSpicy = () => {
+  var spicyItems = document.getElementsByClassName("spicyItem");
+  const show = document.getElementById("spicyCheck").checked;
+  console.log(show);
+  for (var i = 0; i < spicyItems.length; i++) {
+    spicyItems[i].style.display = show ? "list-item" : "none";
+  }
+};
+
+document.getElementById("spicyCheck").addEventListener("click", filterSpicy);
+
+sortMenu(filteredMenu);
+renderAll();
 console.log(filteredMenu);
